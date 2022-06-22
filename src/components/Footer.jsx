54195aa7ea9909a7678d1c3ba7e7608078aa66b3
z851103999 <851103999@qq.com>
2022-06-22 14:50:33 +0800

    bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ const Footer = () => {
   return (
     <footer>
       <a href="/">
-        <img src="../../public/assets/icons/facebook.svg" alt="facebook" />
+        <img src="/assets/icons/facebook.svg" alt="facebook" />
       </a>
       <a href="/">
-        <img src="../../public/assets/icons/instagram.svg" alt="instagram" />
+        <img src="/assets/icons/instagram.svg" alt="instagram" />
       </a>
       <a href="/">
-        <img src="../../public/assets/icons/twitter.svg" alt="twitter" />
+        <img src="/assets/icons/twitter.svg" alt="twitter" />
       </a>
     </footer>
   );
